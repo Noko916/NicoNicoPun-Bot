232bdea04fc6b79872cc21bc8bed433d1b5758fa
Noko <Noko916@users.noreply.github.com>
2020-08-02 03:00:37 +0900

    Update link.js

--- commands/link.js
+++ commands/link.js
@@ -4,11 +4,11 @@ const EmbedLink = new Discord.MessageEmbed()
   .setTitle("リンク一覧")
   .addField(
     "コミュニティ",
-    "[[ １号店 ](https://com.nicovideo.jp/community/co1354820)]　[[２号店](https://com.nicovideo.jp/community/co2645213)]　[[３号店](https://com.nicovideo.jp/community/co3540952)]"
+    "[ １号店 ](https://com.nicovideo.jp/community/co1354820)　[２号店](https://com.nicovideo.jp/community/co2645213)　[３号店](https://com.nicovideo.jp/community/co3540952)"
   )
-  .addField("ポイント表","[[ Here ](https://docs.google.com/spreadsheets/d/1w0ZvpBs0P0e-l-0emYZsQYfRWbBKBtbwsWYAPEd6VyE/edit#gid=977767115)]")
-  .addField("シーズン企画「すごろくツアーズ」","[[ ルール ](https://docs.google.com/document/d/1vwjvLG1LE1dX4StsFqh6tm0820-sDENHMj_-62-XHKc/edit?usp=sharing)]　[[ フィールド ](https://docs.google.com/spreadsheets/d/1XguQP5eG6tKRgjEsYSwZU-ndn-eoi0vYXsVZxBTe08Y/edit#usp=sharing)]")
-  .addField("黒魔法発動中！","[[ 詳細 ](https://docs.google.com/document/d/1XKd5gn0wwrP5teSWy9UAQ2EP8efhU7kBp14hsC-nbDE/edit?usp=sharing)]")
+  .addField("ポイント表","[Spreadsheets](https://docs.google.com/spreadsheets/d/1w0ZvpBs0P0e-l-0emYZsQYfRWbBKBtbwsWYAPEd6VyE/edit#gid=977767115)")
+  .addField("シーズン企画「すごろくツアーズ」","[ルール](https://docs.google.com/document/d/1vwjvLG1LE1dX4StsFqh6tm0820-sDENHMj_-62-XHKc/edit?usp=sharing)　[フィールド](https://docs.google.com/spreadsheets/d/1XguQP5eG6tKRgjEsYSwZU-ndn-eoi0vYXsVZxBTe08Y/edit#usp=sharing)")
+  .addField("黒魔法発動中！","[詳細](https://docs.google.com/document/d/1XKd5gn0wwrP5teSWy9UAQ2EP8efhU7kBp14hsC-nbDE/edit?usp=sharing)")
 
   .setColor(1752220);
 
