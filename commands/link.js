52fab7e04fc42cf2495900c34a4979725ff5bdc8
Noko <Noko916@users.noreply.github.com>
2020-08-15 17:17:43 +0900

    Update link.js

--- commands/link.js
+++ commands/link.js
@@ -8,7 +8,6 @@ const EmbedLink = new Discord.MessageEmbed()
   )
   .addField("ポイント表","　[シート](https://docs.google.com/spreadsheets/d/1w0ZvpBs0P0e-l-0emYZsQYfRWbBKBtbwsWYAPEd6VyE/edit#gid=977767115)")
   .addField("シーズン企画「すごろくツアーズ」","[ルール](https://docs.google.com/document/d/1vwjvLG1LE1dX4StsFqh6tm0820-sDENHMj_-62-XHKc/edit?usp=sharing)　[フィールド](https://docs.google.com/spreadsheets/d/1XguQP5eG6tKRgjEsYSwZU-ndn-eoi0vYXsVZxBTe08Y/edit#usp=sharing)")
-  .addField("ミステリーイベント：黒魔法発動中！","　[詳細](https://docs.google.com/document/d/1XKd5gn0wwrP5teSWy9UAQ2EP8efhU7kBp14hsC-nbDE/edit?usp=sharing)")
 
   .setColor(1752220);
 
