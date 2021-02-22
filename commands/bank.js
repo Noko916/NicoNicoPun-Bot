58de87b146ed40a70d0094b3613ec452446aa8c3
Noko <Noko916@users.noreply.github.com>
2021-02-22 21:57:42 +0900

    Update bank.js

--- commands/bank.js
+++ commands/bank.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 const EmbedBank = new Discord.MessageEmbed()
   .addField("ポイント表 :","[ **Here** ](https://docs.google.com/spreadsheets/d/1w0ZvpBs0P0e-l-0emYZsQYfRWbBKBtbwsWYAPEd6VyE/edit#gid=977767115)")
-  .addField("シーズン企画「アトリエ」","[ルール](https://docs.google.com/document/d/1lXMIvFi7IxdYBtovydpRQteRk36LiJ9Bpjpva1am4yk/edit)　[企画シート]](https://docs.google.com/spreadsheets/d/1s2QfczDzd0bpGneXdl3wmcfbyfjDS4GbjTBKshSnwF8/edit#gid=0)")
+  .addField("シーズン企画「アトリエ」","[[ルール]](https://docs.google.com/document/d/1lXMIvFi7IxdYBtovydpRQteRk36LiJ9Bpjpva1am4yk/edit)　[[企画シート]](https://docs.google.com/spreadsheets/d/1s2QfczDzd0bpGneXdl3wmcfbyfjDS4GbjTBKshSnwF8/edit#gid=0)")
   .setColor(1752220);
 
 exports.run = (client, message, args) => {
