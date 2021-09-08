69a395b2d6c03a499bbcb2df999bbabe05b564d8
Noko <nokohrest@gmail.com>
2021-09-09 05:57:33 +0900

    ５４

--- main.js
+++ main.js
@@ -148,7 +148,7 @@ client.on("message", async message => {
       const RC = message.guild.roles.cache.get(PlayerRole[i]);
       //console.log(RC)
       console.log("------------------------------")
-      console.log(RC.members[0])
+      console.log(RC.members.user)
       console.log("------------------------------")
 
     }
