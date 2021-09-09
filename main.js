8f1b0bcab870793d134972e75f056e56c1d7972b
Noko <nokohrest@gmail.com>
2021-09-09 09:20:19 +0900

    66
    akirameta

--- main.js
+++ main.js
@@ -60,11 +60,12 @@ client.on("message", async message => {
 
 // 大会用
 
-const PlayerRole   = ["880761554633641994", "880761972549890088", "880762186606198824", "880762263311626251", "880762336422551622", "880762421751476226", "880762536285315093"]
-const PlayerChID   = ["880762821070172161", "880762887587655732", "880762936065417286", "880762969137512508", "880763070979391498", "880763122963583008", "880763163648335892"]
-const LogChID      =  "880764948584726588";
-const AnnounceChID =  "880875110322565150";
+const PlayerRole = ["880761554633641994", "880761972549890088", "880762186606198824", "880762263311626251", "880762336422551622", "880762421751476226", "880762536285315093"]
+const PlayerChID = ["880762821070172161", "880762887587655732", "880762936065417286", "880762969137512508", "880763070979391498", "880763122963583008", "880763163648335892"]
+const LogChID = "880764948584726588";
+const AnnounceChID = "880875110322565150";
 
+/*
 client.on("guildMemberUpdate", (oldMember, newMember) => {
 
   console.log("guildMemberUpdate");
@@ -76,14 +77,40 @@ client.on("guildMemberUpdate", (oldMember, newMember) => {
   console.log(newMember.roles._roles.name);
 
 })
+*/
 
 client.on("message", async message => {
 
   const MessageChID = message.channel.id;
-  
+
   if (!PlayerChID.includes(MessageChID) && MessageChID != AnnounceChID) return; // 指定外のチャンネルの発言の場合はreturn;
 
   if (message.author.bot) return;   // botの場合return
+
+  if (message.content == "~upd") {
+    //console.log("Update")
+    //for(var i = 0; i < PlayerChID.length; i++){
+    for (var i = 0; i < 1; i++) {
+
+      //console.log("Updat2e")
+      const RC = message.guild.roles.cache.get(PlayerRole[i]);
+      //console.log(RC)
+      console.log("------------------------------")
+      console.log(RC.members.cache.get("username"));
+      console.log("------------------------------")
+
+    }
+  }
+
+  if (message.content == "~chk") {
+
+    const RC = message.guild.roles.cache.get(PlayerRole[0]);
+
+    console.log("------------------------------")
+    console.log(RC.members)
+    console.log("------------------------------")
+  }
+
 
   // プレイヤーのメッセージをログに収集
   if (PlayerChID.includes(MessageChID)) {
@@ -109,14 +136,13 @@ client.on("message", async message => {
     client.channels.cache.get(LogChID).send(sendEmbed);
   }
 
-
   // アナウンスチャンネルから各プレイヤーチャンネルに一斉にメッセージを送信
-  if(MessageChID == AnnounceChID && message.content.startsWith("+")){
+  if (MessageChID == AnnounceChID && message.content.startsWith("+")) {
 
     const cont = message.content;
     const rep = cont.slice(1);
 
-    const sendEmbed = {        
+    const sendEmbed = {
       embed: {
         author: {
           name: message.member.displayName,
@@ -137,32 +163,6 @@ client.on("message", async message => {
 
     client.channels.cache.get(LogChID).send(sendEmbed)
 
-  }
-
-  //console.log(message.content);
-
-  if(message.content == "~upd"){
-    //console.log("Update")
-    //for(var i = 0; i < PlayerChID.length; i++){
-    for(var i = 0; i < 1; i++){
-
-      //console.log("Updat2e")
-      const RC = message.guild.roles.cache.get(PlayerRole[i]);
-      //console.log(RC)
-      console.log("------------------------------")
-      console.log(RC.members.cache.get("username"));
-      console.log("------------------------------")
-
-    }
-  }
-
-  if(message.content == "~chk"){
-
-      const RC = message.guild.roles.cache.get(PlayerRole[0]);
-
-      console.log("------------------------------")
-      console.log(RC.members)
-      console.log("------------------------------")
   }
 
 });
