5145ecd19fd2c35635a4afecef8eb60a598ba3ca
Noko <nokohrest@gmail.com>
2021-10-16 02:38:38 +0900

    大会モードの切り替え機能の追加

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Discord bot implements
 const Discord = require("discord.js");
 const fs = require('fs');
-const { prefix, version } = require('./config.json');
+const { prefix, version, Mode, isTournament } = require('./config.json');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -59,100 +59,104 @@ client.on("message", async message => {
 */
 
 // 大会用
-/*
-const PlayerRole = ["880761554633641994", "880761972549890088", "880762186606198824", "880762263311626251", "880762336422551622", "880762421751476226", "880762536285315093"]
-const PlayerChID = ["880762821070172161", "880762887587655732", "880762936065417286", "880762969137512508", "880763070979391498", "880763122963583008", "880763163648335892"]
-const LogChID = "880764948584726588";
-const AnnounceChID = "880875110322565150";
+if (isTournament) {
+  const PlayerRole = ["880761554633641994", "880761972549890088", "880762186606198824", "880762263311626251", "880762336422551622", "880762421751476226", "880762536285315093"]
+  const PlayerChID = ["898594296205307924", "898594317281665044", "896698627584561212", "896698653136281640", "896698679807856670", "896698705225342987", "896698724875644948"]
+  const LogChID = "880764948584726588";
+  const AnnounceChID = "880875110322565150";
 
-client.on("message", async message => {
+  client.on("message", async message => {
 
-  const MessageChID = message.channel.id;
+    const MessageChID = message.channel.id;
 
-  if (!PlayerChID.includes(MessageChID) && MessageChID != AnnounceChID) return; // 指定外のチャンネルの発言の場合はreturn;
+    if (!PlayerChID.includes(MessageChID) && MessageChID != AnnounceChID) return; // 指定外のチャンネルの発言の場合はreturn;
 
-  if (message.author.bot) return;   // botの場合return
+    if (message.author.bot) return;   // botの場合return
 
-  if (message.content == "~upd") {
-    //console.log("Update")
-    //for(var i = 0; i < PlayerChID.length; i++){
-    for (var i = 0; i < 1; i++) {
+    if (message.content == "~upd") {
+      //console.log("Update")
+      //for(var i = 0; i < PlayerChID.length; i++){
+      for (var i = 0; i < 1; i++) {
 
-      //console.log("Updat2e")
-      const RC = message.guild.roles.cache.get(PlayerRole[i]);
-      //console.log(RC)
-      console.log("------------------------------")
-      console.log(RC.members.cache.get("username"));
-      console.log("------------------------------")
+        //console.log("Updat2e")
+        const RC = message.guild.roles.cache.get(PlayerRole[i]);
+        //console.log(RC)
+        console.log("------------------------------")
+        console.log(RC.members.cache.get("username"));
+        console.log("------------------------------")
 
-    }
-  }
-
-  if (message.content == "~chk") {
-
-    const RC = message.guild.roles.cache.get(PlayerRole[0]);
-
-    console.log("------------------------------")
-    console.log(RC.members)
-    console.log("------------------------------")
-  }
-
-
-  // プレイヤーのメッセージをログに収集
-  if (PlayerChID.includes(MessageChID)) {
-
-    const sendEmbed = {
-      embed: {
-        author: {
-          name: message.member.displayName,
-          icon_url: message.member.user.displayAvatarURL()
-        },
-        image: {
-          url: message.attachments.map(attachment => attachment.url)[0]
-        },
-        description: message.content,
-        footer: {
-          text: `#${message.channel.name}`
-        },
-        timestamp: message.createdTimestamp
       }
     }
 
-    // 送信
-    client.channels.cache.get(LogChID).send(sendEmbed);
-  }
+    if (message.content == "~chk") {
 
-  // アナウンスチャンネルから各プレイヤーチャンネルに一斉にメッセージを送信
-  if (MessageChID == AnnounceChID && message.content.startsWith("+")) {
+      const RC = message.guild.roles.cache.get(PlayerRole[0]);
 
-    const cont = message.content;
-    const rep = cont.slice(1);
-
-    const sendEmbed = {
-      embed: {
-        author: {
-          name: message.member.displayName,
-          icon_url: message.member.user.displayAvatarURL()
-        },
-        description: rep,
-        footer: {
-          text: `#${message.channel.name}`
-        },
-        color: 15844367,
-        timestamp: message.createdTimestamp
-      }
+      console.log("------------------------------")
+      console.log(RC.members)
+      console.log("------------------------------")
     }
 
-    PlayerChID.forEach(element => {
-      client.channels.cache.get(element).send(sendEmbed)
-    });
 
-    client.channels.cache.get(LogChID).send(sendEmbed)
+    // プレイヤーのメッセージをログに収集
+    if (PlayerChID.includes(MessageChID)) {
 
-  }
+      const sendEmbed = {
+        embed: {
+          author: {
+            name: message.member.displayName,
+            icon_url: message.member.user.displayAvatarURL()
+          },
+          image: {
+            url: message.attachments.map(attachment => attachment.url)[0]
+          },
+          description: message.content,
+          footer: {
+            text: `#${message.channel.name}`
+          },
+          timestamp: message.createdTimestamp
+        }
+      }
 
-});
-*/
+      // 送信
+      client.channels.cache.get(LogChID).send(sendEmbed);
+    }
+
+    // アナウンスチャンネルから各プレイヤーチャンネルに一斉にメッセージを送信
+    if (MessageChID == AnnounceChID && message.content.startsWith("+")) {
+
+      const cont = message.content;
+      const rep = cont.slice(1);
+
+      const sendEmbed = {
+        embed: {
+          author: {
+            name: message.member.displayName,
+            icon_url: message.member.user.displayAvatarURL()
+          },
+          description: rep,
+          footer: {
+            text: `#${message.channel.name}`
+          },
+          color: 15844367,
+          timestamp: message.createdTimestamp
+        }
+      }
+
+      PlayerChID.forEach(element => {
+        client.channels.cache.get(element).send(sendEmbed)
+      });
+
+      client.channels.cache.get(LogChID).send(sendEmbed)
+
+    }
+
+  });
+}
+
+
+/* ----------------------------------------------------------------------------------------------- */
+
 
 // Main
 client.on("message", message => {
@@ -218,7 +222,7 @@ client.on("message", message => {
 });
 
 client.on("ready", () => {
-  client.user.setActivity(`Version: ${version}`, { type: "PLAYING" });
+  client.user.setActivity(`Ver.${version} | ${Mode} Mode `, { type: "PLAYING" });
   console.log("入っているサーバー:");
   var ServerList = client.guilds.cache.map(a => a.name).join(" / ");
   console.log("[ " + ServerList + " ]");
