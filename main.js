fae1b34b3e37ea6b473301f67f9f25e1398e8125
Noko <nokohrest@gmail.com>
2021-11-09 07:06:22 +0900

    #78 a

--- main.js
+++ main.js
@@ -137,14 +137,13 @@ client.on("message", message => {
       .setAuthor(message.member.displayName, message.author.displayAvatarURL())
       .setDescription(message.content)
       .setImage(message.attachments.map(attachment => attachment.url)[0])
-      .setFooter(`${message.guild.name} #${message.channel.name}`, message.guild.iconURL())
+      .setFooter(`[${message.guild.name} #${message.channel.name}](https://discord.com/channels/${m_se}/${m_ch}/${m_id})`, message.guild.iconURL())
       .setTimestamp()
       .setColor(1752220);
 
     client.channels.cache.get('664211602698141696') // nps #2
     //client.channels.cache.get('738003249575100447') // にこにこぷん 運営チーム #ボックス報告
-    .send(TransfarEmbed)
-    .send(`https://discord.com/channels/${m_se}/${m_ch}/${m_id}`);
+    .send(TransfarEmbed);
   }
 
   /* ----------------------------------------------------------------------------------------------- */
