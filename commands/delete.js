08d5071235a1c4d89414aabb84cc054889504f3a
Noko <nokohrest@gmail.com>
2021-11-09 10:34:24 +0900

    #95

--- commands/delete.js
+++ commands/delete.js
@@ -46,7 +46,7 @@ module.exports = {
         } else if (message.content == ".reset") {
 
             for(var c in testChannels){
-                const ch = client.channels.cache.get(String(c));
+                const ch = client.channels.cache.get(c);
                 console.log(ch);
                 //const dMsg = await ch.messages.fetch({ limit: 100 });
                 //message.channel.bulkDelete(dMsg);
