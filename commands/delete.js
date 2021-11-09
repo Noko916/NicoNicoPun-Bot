a42f89c5302679e5e9f835df98de54735465acde
Noko <nokohrest@gmail.com>
2021-11-09 10:55:18 +0900

    #103

--- commands/delete.js
+++ commands/delete.js
@@ -7,7 +7,7 @@ const Channels =
         902551895204331550, 902551906075947008, 902551920323989504, 902551937080242196, 902551950468481064,
         902551962229289040, 902551972199170068];
 
-const testChannels = ['749403949903380560', '664453697425768527', '664453697425768527']
+const testChannels = ['749403949903380560', '664453697425768527', '668726812276293632']
 
 module.exports = {
     name: "delete",
@@ -46,13 +46,8 @@ module.exports = {
         } else if (message.content == ".reset") {
 
             for(var c of testChannels){
-
-                console.log(c)
-
-                client.channels.cache.get(c).send("a");
-
-                //const dMsg = await client.channels.cache.get(String(c)).messages.fetch({ limit: 100 });
-                //message.channel.bulkDelete(dMsg);
+                const dMsg = await client.channels.cache.get(c).messages.fetch({ limit: 100 });
+                message.channel.bulkDelete(dMsg);
             }
         }
     }
